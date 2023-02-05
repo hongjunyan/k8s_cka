999cf5798e4314f7a20dba3462cf3f7245411188
hongjun <hongjunyan@gmail.com>
2023-02-05 16:59:46 +0800

    edit main.py

--- extra/fastapi-docker/README.md
+++ extra/fastapi-docker/README.md
@@ -5,7 +5,7 @@ A backend image for testing
 ## Usage
 - Build Image and push to dockerHub
 ```commandline
-$> docker build hongjunyan/fastapi_backend:v1 .
+$> docker build -t hongjunyan/fastapi_backend:v1 .
 $> docker login
 $> docker push hongjunyan/fastapi_backend:v1
 ```
--- extra/fastapi-docker/app/main.py
+++ extra/fastapi-docker/app/main.py
@@ -15,5 +15,16 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/show_request/")
+def read_root(request: Request):
+    client_host = request.client.host
+    return {"client_host": client_host}
+
+@app.get("/show_request/")
+def read_root(uid, request: Request):
+    client_host = request.client.host
+    return {"client_host": client_host, "uid": uid}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
